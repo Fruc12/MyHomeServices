12ff5ad73a7671916dfc8007848a0a5b8159c91a
Marzoukath <marzoukathagbodja@gmail.com>
2025-05-31 14:36:05 +0200

    ajout

--- MHS_BACK/routes/api.php
+++ MHS_BACK/routes/api.php
@@ -24,10 +24,11 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('categories/{category}/services', [CategoryController::class, 'getCategoryServices']);
     Route::apiResource('services', ServiceController::class)->except(['index']);
     Route::get('reservations/{role}', [ReservationController::class, 'index'])->where('role', 'customer|prestator');
-    Route::apiResource('reservations', ReservationController::class)->except(['index']);
+
     Route::apiResource('rates', RateController::class);
    
     // Route::apiResource('documents', DocumentController::class);
 });
+Route::apiResource('reservations', ReservationController::class)->except(['index']);
 
 // Route::get('/prestators/category/{categoryId}', [PrestatorController::class, 'getPrestatorsByCategory']);
